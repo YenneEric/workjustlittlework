1c893bcb54288b565da8310cdb40f2dc8eec53ab
Nate <nate.carter609@gmail.com>
2024-11-20 17:16:09 -0600

    fixed edit player

--- View/EditPlayer.xaml.cs
+++ View/EditPlayer.xaml.cs
@@ -1,21 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace View
 {
-    public partial class EditPlayer : Page
+    /// <summary>
+    /// Interaction logic for EditPlayer.xaml
+    /// </summary>
+    public partial class EditPlayer : UserControl
     {
         public event EventHandler? NavigateBack;
-
         public EditPlayer()
         {
             InitializeComponent();
         }
 
-        private void BackButton_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            NavigateBack?.Invoke(this, EventArgs.Empty);
+            NavigateBack.Invoke(this, new RoutedEventArgs());
         }
     }
 }
